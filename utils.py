77d53db516f7694347b2f70099466d74bc5b9072
PedroHCN <pedro.hcorradini05@gmail.com>
2025-01-27 14:04:38 -0300

    requirements actualized

--- utils.py
+++ utils.py
@@ -7,9 +7,38 @@ from langchain_community.vectorstores.faiss import FAISS
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
 
 PDF_FOLDER = Path(__file__).parent / 'PDF'
 MODEL_NAME = 'gemini-1.5-flash'
+RETRIEVAL_SEARCH_TYPE = 'mmr'
+RETRIEVAL_PARAMS = {'k': 5, 'fetch_k': 20}
+PROMPT = '''
+Você é um ótimo professor de programação, já atuou durante um tempo como full
+stack no mercado e sabe tudo sobre códigos
+
+Você recebeu diversos documentos sobre programação e deve auxililar o aluno
+com suas duvidas, interprete os documentos e forneça as respostas com base 
+neles
+
+Utilize o contexto para responder as perguntas do usuário. 
+
+Se você não souber a resposta simplesmente diga que não consegue responde-la,
+sempre dê suas respostas em portugues do Brasil independente se houver outras
+linguas no documento fornecido, sempre que houverem termos técnicos preserve 
+a lingua original presente no documento
+
+Contexto:
+{context}
+
+Conversa atual:
+{chat_history}
+
+Human:
+{question}
+
+Ai:
+'''
 
 
 def document_loader():
@@ -54,9 +83,17 @@ def make_chain_conversation():
 
     chat = ChatGoogleGenerativeAI(
         model=MODEL_NAME,
+        search_kwargs=RETRIEVAL_PARAMS
     )
 
-    retriever = vector_store.as_retriever()
+    retriever = vector_store.as_retriever(
+        search_type=RETRIEVAL_SEARCH_TYPE,
+        search_kwargs=RETRIEVAL_PARAMS
+    )
+
+    prompt_template = PromptTemplate.from_template(
+        template=PROMPT
+    )
 
     memory = ConversationBufferMemory(
         return_messages=True,
@@ -68,6 +105,7 @@ def make_chain_conversation():
         memory=memory,
         retriever=retriever,
         return_source_documents=True,
-        verbose=True
+        verbose=True,
+        combine_docs_chain_kwargs={'prompt': prompt_template}
     )
     st.session_state['chain'] = chat_chain
